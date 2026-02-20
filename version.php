36219df72a11003c533b492e1cef81dbd8ef1576
Petr Skoda <petr@mutms.org>
2026-02-20 16:52:37 +0100

    Bump version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'block_muprogmyoverview';
-$plugin->version = 2026021245;
+$plugin->version = 2026022045;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
